aa06de4e7ccb94e8c811e30fe3085866e9a68418
Hjalte Nannestad <hjalte.nannestad@gmail.com>
2024-10-30 12:53:30 +0100

    Initialize the logger

--- src/lib.rs
+++ src/lib.rs
@@ -37,31 +37,20 @@ pub use uuid::Uuid;
 pub use ori_vst_macro::uuid;
 
 #[doc(hidden)]
-pub fn panic_handler(info: &std::panic::PanicInfo) {
-    let backtrace = std::backtrace::Backtrace::capture();
+pub fn install_log() {
+    let layer = tracing_subscriber::fmt();
 
-    let contents = std::fs::read_to_string("/home/anon/ori_panic.log").unwrap_or_default();
-
-    _ = std::fs::write(
-        "/home/anon/ori_panic.log",
-        format!("{}\n\n{}\n\n{}", contents, info, backtrace),
-    );
-
-    eprintln!("{}", info);
-
-    std::process::exit(1);
+    let _ = ori::log::subscriber::set_global_default(layer.finish());
 }
 
 /// Macro for exporting a [`VstPlugin`] and generating the necessary boilerplate.
 #[macro_export]
-macro_rules! vst3 {
+macro_rules! export {
     ($plugin:ty) => {
         #[doc(hidden)]
         const _: () = {
             #[no_mangle]
             unsafe extern "system" fn GetPluginFactory() -> *mut ::std::ffi::c_void {
-                ::std::panic::set_hook(::std::boxed::Box::new($crate::panic_handler));
-
                 let factory = $crate::Factory::<$plugin>::new();
 
                 ::std::boxed::Box::into_raw(factory) as *mut ::std::ffi::c_void
@@ -70,6 +59,8 @@ macro_rules! vst3 {
             #[no_mangle]
             #[cfg(target_os = "linux")]
             extern "system" fn ModuleEntry(_: *mut ::std::ffi::c_void) -> bool {
+                $crate::install_log();
+
                 true
             }
 
@@ -82,6 +73,8 @@ macro_rules! vst3 {
             #[no_mangle]
             #[cfg(target_os = "windows")]
             extern "system" fn InitDll() -> bool {
+                $crate::install_log();
+
                 true
             }
 
@@ -94,6 +87,8 @@ macro_rules! vst3 {
             #[no_mangle]
             #[cfg(target_os = "macos")]
             extern "system" fn bundleEntry(_: *mut ::std::ffi::c_void) -> bool {
+                $crate::install_log();
+
                 true
             }
 
@@ -110,9 +105,11 @@ pub mod prelude {
     //! A prelude for convenience.
 
     pub use crate::{
-        uuid, vst3, Activate, AudioLayout, AudioPort, Bool, Buffer, BufferLayout, Float, Info,
-        Param, ParamFlags, Params, Process, Unit, VstPlugin,
+        Activate, AudioLayout, AudioPort, Bool, Buffer, BufferLayout, Float, Info, Param,
+        ParamFlags, Params, Process, Unit, VstPlugin,
     };
+
+    pub use ori_vst_macro::uuid;
 
     pub use ori::prelude::*;
 }
